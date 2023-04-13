6563c6675e68ff7a95153737773d3c03cc5deae7
Alessandro Allegranzi <allegranzia@gmail.com>
2023-04-12 20:06:08 -0400

    loading input correctly

---
+++ cs_526_term_project_allegranzi/src/Process.java
@@ -0,0 +1,53 @@
+public class Process {
+    private int id;
+
+    private int priority;
+    private int arrivalTime;
+    private int duration;
+
+    public Process(int id, int priority, int arrivalTime, int duration) {
+        this.id = id;
+        this.priority = priority;
+        this.arrivalTime = arrivalTime;
+        this.duration = duration;
+    }
+
+    public Process(int[] inputArray) {
+        this.id = inputArray[0];
+        this.priority = inputArray[1];
+        this.arrivalTime = inputArray[2];
+        this.duration = inputArray[3];
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public int getPriority() {
+        return this.priority;
+    }
+
+    public int getArrivalTime() {
+        return this.arrivalTime;
+    }
+
+    public int getDuration() {
+        return this.duration;
+    }
+
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Id = ");
+        builder.append(this.id);
+        builder.append(", ");
+        builder.append("Priority = ");
+        builder.append(this.priority);
+        builder.append(", ");
+        builder.append("Duration = ");
+        builder.append(this.duration);
+        builder.append(", ");
+        builder.append("Arrival Time = ");
+        builder.append(this.arrivalTime);
+        return builder.toString();
+    }
+}
--- cs_526_term_project_allegranzi/src/ProcessScheduling.java
+++ cs_526_term_project_allegranzi/src/ProcessScheduling.java
@@ -1,5 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ProcessScheduling {
-    public static void main(String args[]) {
-        System.out.println("Hello world");
+    public static void main(String args[]) throws FileNotFoundException {
+        File file = new File(System.getProperty("user.dir") + "/src/input/process_scheduling_input.txt");
+        Scanner reader = new Scanner(file);
+
+        while (reader.hasNextLine()) {
+            String line = reader.nextLine().toString();
+            String[] inputArray = line.split(" ");
+            int[] inputArrayInt  = new int[4];
+            int j = 0;
+            for (int i = 0; i < inputArray.length; i++) {
+                inputArrayInt[i] = Integer.valueOf(inputArray[i]);
+            }
+            Process process = new Process(inputArrayInt);
+            System.out.println(process.toString());
+        }
     }
 }
