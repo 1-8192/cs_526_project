57ba5e1b68ab4a16d1f77e085f17908bbfa3f597
Alessandro Allegranzi <allegranzia@gmail.com>
2023-04-19 17:59:22 -0400

    refactoring

---
+++ cs_526_term_project_allegranzi/src/constants/SchedulerConstants.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class SchedulerConstants {
+    public static int MAX_WAIT_TIME = 30;
+    public static String DIR = "user.dir";
+    public static String READ_FILE_PATH = "/src/input/process_scheduling_input.txt";
+    public static String WRITE_FILE_PATH = "/src/output/process_scheduling_output.txt";
+}
